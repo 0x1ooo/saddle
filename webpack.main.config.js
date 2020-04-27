d96ff79939a43a59b55a18246c0a4f0e2dd80478
Wizcas Chen <chen@wizcas.me>
2020-04-28 00:44:32 +0800

    Update webpack configs to copy vendor files into output dir

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const rules = require('./webpack.rules');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 function srcPaths(src) {
   return path.join(__dirname, src);
@@ -12,8 +14,16 @@ module.exports = {
   entry: './src/main/main.ts',
   // Put your normal webpack config below here
   module: {
-    rules: require('./webpack.rules'),
+    rules,
   },
+  plugins: [
+    ...this.plugins,
+    new CopyWebpackPlugin([{
+      from: path.join(__dirname, 'vendor/**'),
+      to: path.join(__dirname, '.webpack/main'),
+      context: path.join(__dirname, 'vendor'),
+    }]),
+  ],
   resolve: {
     alias: {
       '@main': srcPaths('src/main'),
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,6 +1,7 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 function srcPaths(src) {
   return path.join(__dirname, src);
@@ -19,7 +20,13 @@ module.exports = {
   module: {
     rules,
   },
-  plugins: plugins,
+  plugins: [
+    ...plugins,
+    ...['img'].map(subdir => new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'assets', subdir),
+      to: path.resolve(__dirname, '.webpack/renderer', subdir),
+    }])),
+  ],
   resolve: {
     alias: {
       '@main': srcPaths('src/main'),
