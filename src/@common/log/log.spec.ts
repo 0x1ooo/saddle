2f0f81246ec088d346ff6e3b9854d77df01e82c7
Wizcas Chen <chen@wizcas.me>
2020-04-29 12:57:08 +0800

    Add spec file for log package and test flow for @common

---
+++ src/@common/log/log.spec.ts
@@ -0,0 +1,17 @@
+import { makeLoggerOptions } from '@common/log';
+import { FileAppender } from 'log4js';
+import path from 'path';
+
+it('makes a merged logger options w/ given file path', () => {
+  const customCat = { appenders: [], level: 'trace' };
+  const opts = makeLoggerOptions('a', 'b.log', {
+    categories: {
+      test: customCat,
+    },
+  });
+  expect((opts.appenders.file as FileAppender).filename).toBe(
+    path.join('a', 'b.log')
+  );
+  expect(Object.keys(opts.categories)).toHaveLength(2);
+  expect(opts.categories.test).toEqual(customCat);
+});
